d5f3afc6b2ffb06695daeb2c7f7d7f1c69c33890
appltree <syyoon@appltree.io>
2019-02-04 08:51:39 +0900

    Changed message

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`yongtrim`, `application`, `game`]} />
     <p>
-      2013년 시작한 (주)용트림이 <br />
+      2013년에 시작한 (주)용트림이 <br />
       2019년 지금, 애플트리(ApplTree.io)로 새롭게 시작합니다.
     </p>
     <a href="https://appltree.io">애플트리 홈페이지로 이동</a>
