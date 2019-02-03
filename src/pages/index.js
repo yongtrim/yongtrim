ad20ab9972ac3dd8da37a0431a76ff76f23de2cc
appltree <syyoon@appltree.io>
2019-02-04 08:23:30 +0900

    Changed message

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
       2013년 시작한 (주)용트림이 <br />
       2019년 지금, 애플트리(ApplTree.io)로 새롭게 단장합니다.
     </p>
-    <a href="https://appltree.io">애플트리 홈페이지 이동</a>
+    <a href="https://appltree.io">애플트리 홈페이지로 이동</a>
   </Layout>
 );
 
