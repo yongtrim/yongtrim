a4076956e8ea5ff5539f652ddf4c75ca1aa48112
appltree <syyoon@appltree.io>
2019-02-04 07:41:00 +0900

    Changed appltree address

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>용트림</h1>
     <p>(주)용트림이 애플트리(ApplTree.io)로 새롭게 단장합니다.</p>
-    <a href="http://appltree.io">애플트리 홈페이지 이동</a>
+    <a href="https://appltree.io">애플트리 홈페이지 이동</a>
   </Layout>
 );
 
